1cbb2e2e4440629cddfbf9896043bdad81cb15ba
AMess33 <amess3309@gmail.com>
2023-03-16 13:00:33 -0500

    update server.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   try {
     const categoryData = await Category.findAll({
       include: [
-        { model: Product }
+        { model: Product, as: 'products' }
       ]
     })
     res.status(200).json(categoryData);
